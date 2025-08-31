a1e9addd1ecfd67f343ffb9179c4627c39df8482
ahmed <ahmedsukkary52##@gmail.com>
2025-08-31 20:20:42 +0300

    + Initialize project + Added empty home page + Added colors and route app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:islamy/pages/home_page.dart';
+import 'package:islamy/utils/route_app.dart';
+void main (){
+  runApp(IslamiApp());
+}
+class IslamiApp extends StatelessWidget {
+  const IslamiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoute.homeRoutName,
+      routes: {
+        AppRoute.homeRoutName : (context) => HomePage(),
+      },
+    );
+  }
+}
---
+++ lib/utils/color_app.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+class ColorApp {
+  static const Color primaryColor = Color(0xffE2BE7F);
+  static const Color blackColor = Color(0xff202020);
+  static const Color blackBgColor = Color(0x99202020);
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color transparentColor = Colors.transparent;
+}
